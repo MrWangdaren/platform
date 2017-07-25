ff7e53619bbd2cc1c8ff787edf1b0dbe80cf531f
wangyan <mr0wangyan@163.com>
2017-07-25 15:52:56 +0800

    取出无用类

---
+++ src/main/java/com/dtk/weixin/wxutils/MyX509TrustManager.java
@@ -0,0 +1,24 @@
+package com.dtk.weixin.wxutils;
+
+import java.security.cert.CertificateException;  
+import java.security.cert.X509Certificate;  
+  
+import javax.net.ssl.X509TrustManager;  
+  
+/**
+ * <p>Description:证书信任管理器（用于https请求）  </p>
+ * @author wy
+ * @date 2017年6月21日 上午10:29:08
+ */
+public class MyX509TrustManager implements X509TrustManager {  
+  
+    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {  
+    }  
+  
+    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {  
+    }  
+  
+    public X509Certificate[] getAcceptedIssuers() {  
+        return null;  
+    }  
+}  
